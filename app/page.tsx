3444a35a034c0fd8aead544df7b14998b28b9ff6
guestopenforall <guestopenforall@gmail.com>
2026-02-06 02:35:26 +0500

    Header and Footer adder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import RegisterForm from './appComponets/(auth)/Register'
+import Header from './appComponets/Header'
+import Footer from './appComponets/Footer'
+
 
 const MainPage = () => {
   return (
     <>
+    <div>
+      <Header />
+    </div>
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100 border border-red-600 rounded-lg">
       <h1 className="text-3xl font-bold shadow-lg border rounded-lg p-2 mb-2"> Fill The Register Form</h1>
       <RegisterForm />
     </div>
+    <Footer />
     </>
   )
 }
